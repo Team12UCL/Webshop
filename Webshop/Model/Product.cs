2485f3322337ad906c26dccca57fd39cf4addd42
ikvi63270 <ikvi63270@edu.ucl.dk>
2024-09-05 13:34:35 +0200

    product.cs update

--- Webshop/Model/Product.cs
+++ Webshop/Model/Product.cs
@@ -8,5 +8,12 @@ namespace Webshop.Model
 {
     internal class Product
     {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public float Price { get; set; }
+        public int Stock { get; set; }
+        public string ImagePath { get; set; }
     }
 }
